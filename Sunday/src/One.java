c5008cc02c29a295474424e1765df138b03e4f5e
Arunbabloo123 <arunbaskaran1998@gmail.com>
2019-06-16 10:29:19 -0400

    uu

---
+++ Sunday/src/One.java
@@ -0,0 +1,20 @@
+
+public class One {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Login();  
+	}
+public static void Login()
+{
+	Logout();
+}
+public static void Logout()
+{
+	forgetpassword();
+}
+public static void forgetpassword()
+{
+	int x=9/0;
+}
+}
